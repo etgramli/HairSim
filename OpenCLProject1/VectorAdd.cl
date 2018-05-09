5a440d3d86e18828ca81c81b0034e2709bb6da6a
etgramli <etgramlich@gmx.de>
2018-05-09 16:14:32 +0200

    Added second command queue specifically for kernels and events to synchronize buffer copying with kernel executions

--- OpenCLProject1/VectorAdd.cl
+++ OpenCLProject1/VectorAdd.cl
@@ -2,8 +2,8 @@
 // TODO: Add OpenCL kernel code here.
 
 __kernel void simple_add(__global const int* A,
-                       __global const int* B,
-                       __global int* C){
+                         __global const int* B,
+                         __global int* C){
     unsigned int i = get_global_id(0);
     C[i] = A[i] + B[i];
 }
--- OpenCLProject1/main.cpp
+++ OpenCLProject1/main.cpp
@@ -4,12 +4,14 @@
 #include <fstream>
 #include <CL/cl.hpp>
 
-const int ARRAYSIZE = 256;
-int A[ARRAYSIZE] = {1};
-int B[ARRAYSIZE] = {5};
+const long ARRAYSIZE = 1024*1024;
+int A[ARRAYSIZE];
+int B[ARRAYSIZE];
 int C[ARRAYSIZE];
 
+// Create a context for the specified platform and the stated device type
 cl::Context createContext(cl_device_type type, cl::Platform *platform);
+bool validate();
 
 
 int main() {
@@ -70,7 +72,9 @@ int main() {
     // Create cl::Kernel
     cl::Kernel kernel(program, "simple_add", &err);
     // Create cl::ConandQueue
-    cl::CommandQueue queue(context, devices[0], 0, &err);
+    // If using multiple queues you must synchronize with events
+    cl::CommandQueue queueIO(context, devices[0], 0, &err);     // Queue for IO (reading and writing buffers)
+    cl::CommandQueue queueKernel(context, devices[0], 0, &err); // Queue for Kernels (execution)
     
     // Create cl::Event
     cl::Event event;
@@ -83,29 +87,25 @@ int main() {
     cl::Buffer bufB(context, CL_MEM_READ_ONLY, sizeof(int) * ARRAYSIZE);
     cl::Buffer bufC(context, CL_MEM_WRITE_ONLY, sizeof(int) * ARRAYSIZE);
     std::cout << "Copying Buffers from Host to Device" << std::endl;
-    cl::Event copyToDevice;
-    queue.enqueueWriteBuffer(bufA, CL_TRUE, 0, sizeof(int) * ARRAYSIZE, (void *) A);
-    queue.enqueueWriteBuffer(bufB, CL_TRUE, 0, sizeof(int) * ARRAYSIZE, (void *) B, NULL, &copyToDevice);
+    cl::Event copyBufferA;
+    cl::Event copyBufferB;
+    std::vector<cl::Event> copyHostToDeviceEvents;
+    queueIO.enqueueWriteBuffer(bufA, CL_TRUE, 0, sizeof(int) * ARRAYSIZE, (void *) A, NULL, &copyBufferA);
+    queueIO.enqueueWriteBuffer(bufB, CL_TRUE, 0, sizeof(int) * ARRAYSIZE, (void *) B, NULL, &copyBufferB);
+    copyHostToDeviceEvents.push_back(copyBufferA);  // PUSHBACK OF EVENTS MUST BE AFTER ENQUEING !!!
+    copyHostToDeviceEvents.push_back(copyBufferB);
     kernel.setArg(0, bufA);
     kernel.setArg(1, bufB);
     kernel.setArg(2, bufC);
     std::cout << "Enqueue Vecotr add kernel" << std::endl;
-    copyToDevice.wait();
-    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(ARRAYSIZE), cl::NullRange, NULL, &event);
-    
-    // Wait
-    event.wait();
-    std::cout << "Vecotr add kernel completed" << std::endl;
-
+    queueKernel.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(ARRAYSIZE), cl::NullRange, &copyHostToDeviceEvents, &event);
     // Copy back Buffer and print
     cl::Event copyBack;
-    queue.enqueueReadBuffer(bufC, CL_TRUE, 0, sizeof(int) * ARRAYSIZE, C, NULL, &copyBack);
-    copyBack.wait();
+    std::vector<cl::Event> copyBackWatiEvent;
+    copyBackWatiEvent.push_back(event);
+    queueIO.enqueueReadBuffer(bufC, CL_TRUE, 0, sizeof(int) * ARRAYSIZE, C, &copyBackWatiEvent, &copyBack);
 
-    for (int i = 0; i < ARRAYSIZE; ++i) {
-        std::cout << C[i] << ", ";
-    }
-    std::cout << std::endl << std::endl;
+    std::cout << "Is result correct: " << (validate()?"yes":"false") << std::endl << std::endl;
 
     } catch (std::exception& e) {
         std::cout << e.what() << std::endl;
@@ -123,9 +123,8 @@ cl::Context createContext(cl_device_type type, cl::Platform *platform) {
     cl_int err;
     cl::Context context;
     context = clCreateContextFromType(props, type, NULL, NULL, &err);
-    //context = cl::Context(type, props, NULL, NULL, &err);
 
-    // ToDo: Analyze err
+    std::cout << "Create context: ";
     switch (err) {
     case CL_SUCCESS:
         std::cout << "CL Success" << std::endl;
@@ -152,4 +151,15 @@ cl::Context createContext(cl_device_type type, cl::Platform *platform) {
     }
 
     return context;
+}
+
+
+bool validate() {
+    for (long l = 0; l < ARRAYSIZE; ++l) {
+        if (C[l] != A[l] + B[l]) {
+            std::cout << "Wrong value at position: " << l << " (value: " << C[l] << " = " << A[l] << "+" << B[l] << ")" << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
