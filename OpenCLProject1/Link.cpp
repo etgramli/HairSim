f6b51c07a77f7779a241d02fb314f304d5f79a8a
etgramli <etgramlich@gmx.de>
2018-06-15 17:32:47 +0200

    Corrected constants

--- OpenCLProject1/Link.cpp
+++ OpenCLProject1/Link.cpp
@@ -10,27 +10,20 @@ Node* Link::getEnd() const {
     return end;
 }
 
-float Link::getRestLength() const {
-    return restLength;
-}
-
 Vector Link::getSpringForce() {
     Vector force = Vector(0,0,0); // Force from link applied to nodes
 
-	float threshold = 0.05*length;
-
     Vector diff = end->minus(begin);    // Difference from Node positions
 	float diffLength = diff.length();
+    
     if (diffLength > length + threshold) {           // Nodes of hair further away than length of hair -> must be pushed together
-		diff.normalize();
-        force = diff * springConstant * abs(length - diffLength);
+		force = diff * springConstant * abs(length - diffLength);
     } else if (diffLength < length - threshold) {   // Nodes of hair closer togehter than length of hair, must be pushed away
-		diff.normalize();
-        force = diff * springConstant * -1 * abs(length - diffLength);
+		force = diff * springConstant * abs(length - diffLength) * -1;
     }
 
 	if (isnan(force.getX()) || isnan(force.getY()) || isnan(force.getZ())) {
-		force = Vector(0, 0, 0);
+		return Vector(0, 0, 0);
 	}
 	//std::cout << "SpringForce: (" << force.getX() << ", " << force.getY() << ", " << force.getZ() << ")" << std::endl;
 
